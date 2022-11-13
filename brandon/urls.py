4466ffd91be642b3ee67477ab4b1df9476ce6594
mercelisvaughan <55514935+mercelisvaughan@users.noreply.github.com>
2022-11-13 14:24:43 -0500

    Update urls.py

--- brandon/urls.py
+++ brandon/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
 ]
