6a0b103d42e6e9e8345c619874c0f5a01089b739
mercelisvaughan <mercelisvaughan@gmail.com>
2022-11-13 18:10:02 -0500

    able to visit 8000/

--- brandon/urls.py
+++ brandon/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
 ]
--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('index.html', views.index, name="index"),
 ]
 
